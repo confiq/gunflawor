fe8fc540304deb5fd3f4cc9abe71b714a57eb984
Igor Konforti <igor.konforti@fundbox.com>
2018-05-12 23:27:31 +0300

    Finishing with BOOM💥 lib.

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import docker
 import logging
 import requests
+
 from time import sleep
+from boom import boom
+
+# CONFIGURATIONS:
+## StressTest
+BOOM_REQUESTS = 1000  # Number of request that it will send to app
+BOOM_CONCURRENCY = 10
+
+## misc
+GUNICORN_WORKERS = 5  # default workers when starting gunicorn
+# endregion
 
 class Benchmark(object):
-    GUNICORN_WORKERS = 5
+
     DOCKER_IMAGE = 'gunflawor:latest'
+    # overwriting default configs
     LOADS = {
-        'normal':   '',
-        'cpu':      '',
-        'memory':   '',
-        'sleep':    '',
-        'slow_api': '',
+        'normal': '',
+        'cpu': '',
+        # 'memory': '',
+        # 'sleep': '',
+        # 'slow_api': '',
     }
     WORKER_CLASS = {
-        'sync':     {
+        'sync': {
             'gunicorn_workers': GUNICORN_WORKERS * 10
         },
         'eventlet': {},
-        'gevent':   {},
+        'gevent': {},
         # 'gthread',  # python3
         # 'gaiohttp'
         # TODO: gevent.wsgi
@@ -34,7 +46,7 @@ class Benchmark(object):
             self.results[worker_class] = {}
             for load in self.LOADS:
                 run_string = 'gunicorn -b 0.0.0.0:9080 --workers={gunicorn_workers} app.{load}.app:app --worker-class={worker_class}'.\
-                    format(gunicorn_workers=self.get_custom_config(worker_class, 'gunicorn_workers', self.GUNICORN_WORKERS),
+                    format(gunicorn_workers=self.get_custom_config(worker_class, 'gunicorn_workers', GUNICORN_WORKERS),
                            load=load, worker_class=worker_class)
                 container = self.docker_client.containers.run(self.DOCKER_IMAGE, run_string, detach=True,
                                                               ports={'9080/tcp': 9080})
@@ -52,7 +64,6 @@ class Benchmark(object):
             return default
 
     def is_container_alive(self, container):
-        res = None
         container.reload()
         if container.status != 'running':
             raise RuntimeError('Docker {id} is not running. The logs of container is: {logs}'.
@@ -60,18 +71,21 @@ class Benchmark(object):
         for retries in xrange(1, 10):
             try:
                 res = requests.get('http://127.0.0.1:9080/')
+                if not res and not res.ok:
+                    logging.error('The docker did not return OK')
                 break
             except requests.exceptions.ConnectionError:
                 logging.info('retrying to connect to container {id}'.format(id=container.short_id))
                 continue
-
-        if not res and not res.ok:
+        else:
             raise RuntimeError('Docker {id} did not respond to request. Logs: {logs}'.
                                format(id=container.short_id, logs=container.logs()))
 
     def stress_test(self):
-        # TODO: BOOM!
-        return 100
+        res = boom.load('http://127.0.0.1:9080/', BOOM_REQUESTS, BOOM_CONCURRENCY, None, 'GET', '', ct='text/plain',
+                        auth=None, quiet=True)
+        res = boom.calc_stats(res)
+        return round(res.rps, 2)
 
 
 if __name__ == "__main__":
